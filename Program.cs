563cd08435df5448ac1e45efd266f68f4da178fd
arthu <arthu@DESKTOP-NRI1H1V>
2023-03-02 01:45:12 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace FibonacciSequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lastNumber = 0, holder = 1, fibonacciSum = 0;
+
+            Console.Write("Informe o número a ser pesquisado na sequência de Fibonacci: ");
+            int answer = int.Parse(Console.ReadLine());
+
+            while(fibonacciSum < answer)
+            {
+                fibonacciSum = lastNumber + holder;
+                lastNumber = holder;
+                holder = fibonacciSum;
+            }
+
+            if (fibonacciSum == answer)
+            {
+                Console.WriteLine("Este número pertence a sequência!");
+            }
+            else
+            {
+                Console.WriteLine("Este número não pertence a sequência!");
+            }
+        }
+    }
+}
